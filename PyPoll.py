7ac63dcf0961a11e8eb8a3757a994372fd4167c9
clinnegan <clinnegan@gmail.com>
2021-02-27 23:40:05 +1100

    Writing to text file

--- PyPoll.py
+++ PyPoll.py
@@ -24,5 +24,15 @@ print("-----------------")
 print("Winner: " + winner)
 print("-----------------")
 
-    
+text_file = open("PyPoll.txt","w")
+text_file.write("Election Results\n")
+text_file.write("-----------------\n")
+text_file.write("Total Votes: " + str(Total_Votes)+"\n")
+text_file.write("-----------------\n")   
 
+for key,val in v.items():
+    text_file.write((key + ": " + str(round((val/Total_Votes)*100)) + "% " + "(" + str(val) + ")\n"))
+
+text_file.write("-----------------\n")
+text_file.write("Winner: " + winner + "\n")
+text_file.write("-----------------\n")
