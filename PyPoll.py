827d73232449c909896f158a03282148c7d14c6b
clinnegan <clinnegan@gmail.com>
2021-02-27 23:07:27 +1100

    final

--- PyPoll.py
+++ PyPoll.py
@@ -8,5 +8,21 @@ Total_Votes = len(df)
 Total_Votes 
 
 Votes_per_Candidate = df['Candidate'].value_counts()
-Votes_per_Candidate
+v = Votes_per_Candidate.to_dict()
+
+
+print("Election Results")
+print("-----------------")
+print("Total Votes: " + str(Total_Votes))
+print("-----------------")
+
+for key,val in v.items():
+    print(key,": ",str(round((val/Total_Votes)*100)) + "%","(",val,")")
+
+winner = max(v,key=v.get)
+print("-----------------")
+print("Winner: " + winner)
+print("-----------------")
+
+    
 
